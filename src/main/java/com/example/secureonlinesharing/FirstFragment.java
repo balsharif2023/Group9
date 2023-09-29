18e0919ae127b4a150efd86e7bb26e43b93e1310
Jay <jpolanco2017@fau.edu>
2023-09-29 11:32:26 -0400

    Demo three fragments + transitions

---
+++ src/main/java/com/example/secureonlinesharing/FirstFragment.java
@@ -0,0 +1,110 @@
+package com.example.secureonlinesharing;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.secureonlinesharing.databinding.FragmentFirstBinding;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        getActivity().setTitle("Olympus");
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                NavHostFragment.findNavController(FirstFragment.this)
+                  .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                // getting a new volley request queue for making new requests
+               /* RequestQueue volleyQueue = Volley.newRequestQueue(getActivity());
+                // url of the api through which we get random dog images
+                String url = "https://innshomebase.com/securefilesharing/develop/battus/v1/controller/registerLogin/registerLogin.php?userId=1";
+
+                // since the response we get from the api is in JSON, we
+                // need to use `JsonObjectRequest` for parsing the
+                // request response
+              //  try {
+
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                            // we are using GET HTTP request method
+                            Request.Method.GET,
+                            // url we want to send the HTTP request to
+                            url,
+                            // this parameter is used to send a JSON object to the
+                            // server, since this is not required in our case,
+                            // we are keeping it `null`
+                          //  new JSONObject("{userId:1}"),
+                            null,
+
+                            // lambda function for handling the case
+                            // when the HTTP request succeeds
+                            (Response.Listener<JSONObject>) response -> {
+                                // get the image url from the JSON object
+                                String message;
+                                try {
+                                    message = response.getString("message");
+                                    // load the image into the ImageView using Glide.
+                                     //binding.textviewFirst.setText(message);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            },
+
+                            // lambda function for handling the case
+                            // when the HTTP request fails
+                            (Response.ErrorListener) error -> {
+                                // make a Toast telling the user
+                                // that something went wrong
+                                //     Toast.makeText(getActivity(), "Some error occurred! Cannot fetch dog image", Toast.LENGTH_LONG).show();
+                                // log the error message in the error stream
+                                //    Log.e("MainActivity", "loadDogImage error: ${error.localizedMessage}");
+                            }
+                    );
+                    // add the json request object created above
+                    // to the Volley request queue
+                    volleyQueue.add(jsonObjectRequest);
+                //} // catch(JSONException e){} */
+
+            }
+
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ src/main/java/com/example/secureonlinesharing/ThirdFragment.java
@@ -0,0 +1,64 @@
+package com.example.secureonlinesharing;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.secureonlinesharing.databinding.FragmentThirdBinding;
+
+public class ThirdFragment extends Fragment {
+
+    private FragmentThirdBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+
+    )
+
+    {
+
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+        VideoView videoView = (VideoView) binding.videoView;  //casting to VideoView is not Strictly required above API level 26
+        videoView.setZOrderOnTop(true);
+        videoView.setVisibility(View.VISIBLE);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.video_clip)); //set the path of the video that we need to use in our VideoView
+
+        MediaController mediaController = new MediaController(getActivity());
+        mediaController.setAnchorView(videoView);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+        //videoView.setVideoPath("android.resource://" + getActivity().getPackageName() + "/" + R.raw.video_clip); //set the path of the video that we need to use in our VideoView
+        videoView.start();  //start() method of the VideoView class will start the video to play
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+      /*  binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });*/
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
